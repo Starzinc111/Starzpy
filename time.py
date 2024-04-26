79c297158967a408b04d65f6503d5d58a95d137d
Starzinc111 <168036226+Starzinc111@users.noreply.github.com>
2024-04-26 17:35:07 -0400

    Create time.py

---
+++ time.py
@@ -0,0 +1,12 @@
+import time
+import random
+from sympy import isprime
+def main():
+  while true:
+    num=random.randint(1,100)
+    prime_status="prime" if isprime(num) else "not prime"
+    print ("(num) is (prime_status).")
+    time.sleep(10)
+    if __name__="__main__":
+    main()
+    
